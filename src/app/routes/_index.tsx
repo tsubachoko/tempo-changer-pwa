36905a3438fa279fa54966f51bf611efaea336d8
chizzo <57238328+tsubachoko@users.noreply.github.com>
2025-02-24 17:23:22 +0900

    サンプル機能の実装

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,16 +1,44 @@
 import { Box } from '@radix-ui/themes'
 import type { MetaFunction } from '@remix-run/cloudflare'
+import { useRef, useState } from 'react'
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: 'テンポ変換プレイヤー' },
-  ]
+  return [{ title: 'テンポ変換プレイヤー' }]
 }
 
 export default function Index() {
+  const [audioSrc, setAudioSrc] = useState('')
+  const [tempo, setTempo] = useState(100)
+
+  const player = useRef<HTMLAudioElement>(null)
+
+  const audioInput = (event: any) => {
+    var file = event.currentTarget.files[0]
+    var objectUrl = URL.createObjectURL(file)
+    setAudioSrc(objectUrl)
+  }
+
+  const changeTempo = (event: any) => {
+    if (player.current) {
+      setTempo(event.currentTarget.value)
+      player.current.playbackRate = event.currentTarget.value / 100
+    }
+  }
+
   return (
     <Box>
-      サンプル
+      <input type="file" onChange={audioInput} />
+      <div>
+        <input
+          type="range"
+          min="50"
+          max="200"
+          value={tempo}
+          onChange={changeTempo}
+        />
+        <p>再生速度 ×{tempo / 100}</p>
+      </div>
+      <audio src={audioSrc} controls ref={player}></audio>
     </Box>
   )
 }
