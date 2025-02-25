be51e55af9f717295847cb5a12e90578efaab86e
chizzo <57238328+tsubachoko@users.noreply.github.com>
2025-02-25 20:58:23 +0900

    PWAのインストールボタンの設置（素のJS）

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,6 +1,6 @@
-import { Box } from '@radix-ui/themes'
+import { Box, Button } from '@radix-ui/themes'
 import type { MetaFunction } from '@remix-run/cloudflare'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'テンポ変換プレイヤー' }]
@@ -25,20 +25,43 @@ export default function Index() {
     }
   }
 
+  const installPromptEvent = useRef<any>(null)
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (event) => {
+      // Chrome67以前で自動的にプロンプトを表示しないようにする?
+      event.preventDefault();
+
+      // イベントを変数に保存する
+      installPromptEvent.current = event
+    });
+  }, [])
+
+  const install = () => {
+    installPromptEvent?.current.prompt()
+  }
+
   return (
     <Box>
-      <input type="file" onChange={audioInput} />
-      <div>
-        <input
-          type="range"
-          min="50"
-          max="200"
-          value={tempo}
-          onChange={changeTempo}
-        />
-        <p>再生速度 ×{tempo / 100}</p>
-      </div>
-      <audio src={audioSrc} controls ref={player}></audio>
+      <Box>
+        <input type="file" onChange={audioInput} />
+        <div>
+          <input
+            type="range"
+            min="50"
+            max="200"
+            color='red'
+            value={tempo}
+            onChange={changeTempo}
+          />
+          <p>再生速度 ×{tempo / 100}</p>
+        </div>
+        <audio src={audioSrc} controls ref={player}></audio>
+      </Box>
+
+      <Box mt="8">
+        <Button onClick={install}>アプリのインストール</Button>
+      </Box>
     </Box>
   )
 }
